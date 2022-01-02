d4f3c0b5bd9e01848aeef3e15b143967951499be
midlaj <midlajk18@gmail.com>
2022-01-02 22:17:53 +0530

    bill filter

--- controller/addbill.js
+++ controller/addbill.js
@@ -47,7 +47,7 @@ exports.addcoffeebill = (req, res) => {
 
 }
 exports.addpepperbill = (req, res) => {
-    console.log("here")
+
     var pepperbill = new Pepper_bill({
         seller: req.body.seller,
         product: [{
@@ -65,15 +65,27 @@ exports.addpepperbill = (req, res) => {
             category: req.body.hint,
         }]
     })
-    pepperbill.save((err, docs) => {
-        console.log(docs)
+    pepperbill.save((docs, err) => {
+
         res.redirect('/addbill')
     })
 
 
 }
 exports.coffeebill = (req, res) => {
-    Coffee_bill.aggregate([{ $unwind: "$product" }]).then(docs => {
+    var end = new Date()
+    var start = new Date()
+    end.setDate(end.getDate() + 1);
+    start.setDate(start.getDate() - 1);
+    Coffee_bill.aggregate([{ $unwind: "$product" }, {
+        $match: {
+
+            "product.date": {
+                $lt: end,
+                $gte: start
+            }
+        }
+    }]).then(docs => {
         res.render('coffeebill', {
             mainpath: '/viewbill',
             errorMessage: "",
@@ -82,16 +94,100 @@ exports.coffeebill = (req, res) => {
             start: [],
             end: [],
             names: [],
-            seller: docs
+            seller: docs,
+
         })
 
     })
 
 
 }
+exports.filtercoffeebill = (req, res) => {
+
+    var end = new Date()
+    var start = new Date()
+
+    var filter = [{ $unwind: "$product" }]
+    if (req.body.type == 'filter') {
+
+        start = new Date(req.body.sdate);
+        end = new Date(req.body.edate);
+
+        filter = [{ $unwind: "$product" }, {
+            $match: {
+
+                "product.date": {
+                    $lt: end,
+                    $gte: start
+                }
+            }
+        }]
+
+    } else if (req.body.type == 'yestersay') {
+
+        start.setDate(start.getDate() - 2);
+        end.setDate(end.getDate() - 2);
+        filter = [{ $unwind: "$product" }, {
+            $match: {
+
+                "product.date": {
+                    $lt: end,
+                    $gte: start
+                }
+            }
+        }]
+    } else if (req.body.type == '1month') {
+        start.setMonth(start.getMonth() - 1);
+        filter = [{ $unwind: "$product" }, {
+            $match: {
+
+                "product.date": {
+                    $lt: end,
+                    $gte: start
+                }
+            }
+        }]
+
+    } else {
+        start = new Date(08 / 03 / 2000)
+        filter = [{ $unwind: "$product" }]
+
+    }
+
+    Coffee_bill.aggregate(filter).sort({ "product.date": -1, "product._id": -1 }).exec((err, data) => {
+
+        res.render('coffeebill', {
+            mainpath: '/viewbill',
+            errorMessage: "",
+            loads: '',
+            doc: '',
+            start: [],
+            end: [],
+            names: [],
+            seller: data,
+            start: start,
+            end: end,
+
+        })
+    })
+
+
+}
 exports.pepperbill = (req, res) => {
-    Pepper_bill.aggregate([{ $unwind: "$product" }]).then(docs => {
-        console.log(docs)
+    var end = new Date()
+    var start = new Date()
+    end.setDate(end.getDate() + 1);
+    start.setDate(start.getDate() - 1);
+    Pepper_bill.aggregate([{ $unwind: "$product" }, {
+        $match: {
+
+            "product.date": {
+                $lt: end,
+                $gte: start
+            }
+        }
+    }]).then(docs => {
+
         res.render('pepperbill', {
             mainpath: '/viewbill',
             errorMessage: "",
@@ -103,5 +199,76 @@ exports.pepperbill = (req, res) => {
             seller: docs
         })
     })
+
+}
+exports.filterpepperbill = (req, res) => {
+
+    var end = new Date()
+    var start = new Date()
+
+    var filter = [{ $unwind: "$product" }]
+    if (req.body.type == 'filter') {
+
+        start = new Date(req.body.sdate);
+        end = new Date(req.body.edate);
+
+        filter = [{ $unwind: "$product" }, {
+            $match: {
+
+                "product.date": {
+                    $lt: end,
+                    $gte: start
+                }
+            }
+        }]
+
+    } else if (req.body.type == 'yestersay') {
+
+        start.setDate(start.getDate() - 2);
+        end.setDate(end.getDate() - 1);
+        filter = [{ $unwind: "$product" }, {
+            $match: {
+
+                "product.date": {
+                    $lt: end,
+                    $gte: start
+                }
+            }
+        }]
+    } else if (req.body.type == '1month') {
+        start.setMonth(start.getMonth() - 1);
+        filter = [{ $unwind: "$product" }, {
+            $match: {
+
+                "product.date": {
+                    $lt: end,
+                    $gte: start
+                }
+            }
+        }]
+
+    } else {
+        start = new Date(08 / 03 / 2000)
+        filter = [{ $unwind: "$product" }]
+
+    }
+
+    Pepper_bill.aggregate(filter).sort({ "product.date": -1, "product._id": -1 }).exec((err, data) => {
+
+        res.render('pepperbill', {
+            mainpath: '/viewbill',
+            errorMessage: "",
+            loads: '',
+            doc: '',
+            start: [],
+            end: [],
+            names: [],
+            seller: data,
+            start: start,
+            end: end,
+
+        })
+    })
+
 
 }
--- routes/bill.js
+++ routes/bill.js
@@ -7,6 +7,8 @@ const mainController = require('../controller/addbill');
 router.get('/addbill', mainController.addbill);
 router.post('/addcoffeebill', mainController.addcoffeebill);
 router.get('/coffeebill', mainController.coffeebill);
+router.post('/filtercoffeebill', mainController.filtercoffeebill)
+router.post('/filterpepperbill', mainController.filterpepperbill)
 router.get('/pepperbill', mainController.pepperbill);
 router.post('/addpepperbill', mainController.addpepperbill);
 module.exports = router;
